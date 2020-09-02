494fcdd181850fedcceb8c6cb161723c57fc045d
xanax1337 <70622310+xanax1337@users.noreply.github.com>
2020-09-02 00:06:27 -0500

    Add files via upload
    stacey are skids

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+# keep_alive.py
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+    return 'running'
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
